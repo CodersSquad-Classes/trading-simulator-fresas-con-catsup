e7c2d53b8e83675e059eecfd93921822c5b29ff1
evelyn267 <a01646895@tec.mx>
2025-11-17 11:58:42 -0600

    Update orderbook.h

--- orderbook.h
+++ orderbook.h
@@ -1,1 +1,58 @@
+// This file defines the OrderBook class where it stores buy or sell orders using priority queues and executes when the price cross
+
+#ifndef ORDERBOOK_H
+#define ORDERBOOK_H
+
+#include "order.h"
+#include <queue>
+#include <vector>
+#include <mutex>
+#include <string>
+
+struct TopRow {
+    double price; //best price at this level
+    uint64_t qty; //quantity of top order
+    OrderId earliest_order_id;
+};
+
+class OrderBook {
+public:
+    explicit OrderBook(const std::string& symbol);
+    ~OrderBook();
+
+    //add a new order to the appropriate queue.
+    void add_order(const Order& o);
+
+    //match BUY and SELL orders 
+    void match();
+
+    //get top n levels
+    std::vector<TopRow> top_buys(size_t n);
+    std::vector<TopRow> top_sells(size_t n);
+
+    uint64_t total_trades_executed() const;
+
+private:
+    //comparator for buy priority queue
+    struct BuyCmp;
+
+    //comparator for sell priority queue
+    struct SellCmp;
+
+    std::string symbol_;
+
+    //priority queues storing orders
+    std::priority_queue<Order, std::vector<Order>, BuyCmp> buys_;
+    std::priority_queue<Order, std::vector<Order>, SellCmp> sells_;
+
+    mutable std::mutex mtx_;   
+    uint64_t trades_executed_; 
+
+    //for top N values
+    template<typename Cmp>
+    static std::vector<TopRow> snapshot_pq(
+        std::priority_queue<Order, std::vector<Order>, Cmp> pq, size_t n);
+};
+
+#endif // ORDERBOOK_H
 
